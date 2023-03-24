c20b7c729f5837fb62dfcf83ae2f3ff3943afba9
Pavel Dotsenko <pavel_dotsenko@hotmail.com>
2023-03-24 15:42:14 +0600

    fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rela.MixProject do
   def project do
     [
       app: :rela,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.13",
       deps: deps(),
       start_permanent: Mix.env == :prod,
